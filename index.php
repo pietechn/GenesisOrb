6de52907ec33ebe95e0c76d537c909c87c67a0c0
pietechn <pietechnology7@gmail.com>
2023-11-13 15:56:23 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>GenesisOrb</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/Icon.png" rel="icon">
+  <link href="assets/img/Icon.png" rel="Genesis-Orb-Icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css" integrity="sha512-oAvZuuYVzkcTc2dH5z1ZJup5OmSQ000qlfRvuoTTiyTBjwX1faoyearj8KdMq0LgsBTHMrRuMek7s+CxF8yE+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/10.3.1/swiper-bundle.min.css" integrity="sha512-UV9ujyMxyYubOSkCa8+OGzknJ1EilA19WPimPseyMcZaGIoO8l7iNphD0Mq/0R/lNkzBH70ai3tmurxAW0M2ww==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: SoftLand
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.html"><span><img src="assets/img/Icon.png"/></span> Genesis Orb</a></h1>
+  </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <form method="post" action="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTJFkslxGHPVDtnwlwwgDMZWwtTkNZkJvlTFWmVSWJRXRtwTBjTRXWXZNXVCKwtJgzlqcL">
+          <li><input type="submit" style="text-transform: uppercase; color: rgb(161, 107, 46, 0.836); font-weight:bold; background:none; border:none;" value="contact Us"></li>
+          </form>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section class="hero-section" id="hero">
+
+  
+    <div class="wave">
+
+      <svg width="100%" height="255px" viewBox="0 0 1920 355" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+            <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,757 L1017.15166,757 L0,757 L0,439.134243 Z" id="Path"></path>
+          </g>
+        </g>
+      </svg>
+
+    </div>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 hero-text-image">
+          <div class="row" style="margin-top: -195px;">
+            <div class="col-lg-8 text-center text-lg-start">
+              <h1 data-aos="fade-right" style="text-transform: capitalize;">We are on a mission to innovate local musical instruments</h1>
+              <p class="" data-aos="fade-right" data-aos-delay="100" style="text-transform: capitalize;">using cutting-edge and eco-friendly renewable materials.</p>
+              <p data-aos="fade-right" data-aos-delay="200" data-aos-offset="-500">
+              <form method="post" action="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTJFkslxGHPVDtnwlwwgDMZWwtTkNZkJvlTFWmVSWJRXRtwTBjTRXWXZNXVCKwtJgzlqcL">
+                <button class="btn btn-outline-white" style="" type="submit">Contact Us</button>
+              </form>
+            </p>
+            </div>
+            <div class="col-lg-4 iphone-wrap">
+              <!-- <img src="assets/img/pngegg (1).png" alt="Image" class="phone-1" data-aos="fade-right"> -->
+              <img src="assets/img/pngegg.png" alt="Image" class="phone-2" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Home Section ======= -->
+    <section class="section">
+      <div class="container">
+
+        <div class="row justify-content-center text-center">
+          <div class="col-md-5" data-aos="fade-up">
+            <h4 class="section-heading"><span> <i class="bi bi-people"></i></span> WHO WE ARE</h4>
+          </div>
+        </div>.
+        <div class="row justify-content-center text-center mt-2">
+          <div class="col-md-8" data-aos="fade-up">
+            <h4 class=" feature-1" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">GenesisOrb is the first indigenous 
+              musical instrument company in 
+              Africa with the goal to innovate and 
+              secure global market, for indigenous 
+              musical instruments.</h4>
+              </div>
+          </div>
+        </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+
+    <section class="section">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-8 order-2 mt-4" data-aos="fade-left">
+            <center><p class="btn col-md-10" style="text-transform: uppercase; background-color: rgba(161, 107, 46, 0.836); color: white; font-size: 1.5em; font-weight: bold;border-radius: 1em;">Anticipate </p></center>
+            <h3 class="mb-4" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold; color: rgb(12, 110, 12);">The Electronic Talking Drum</h3>          
+          </div>
+          <div class="col-md-2" data-aos="fade-right">
+            <img src="assets/img/drum.jpg" alt="Image" class="img-fluid">
+          </div>
+        </div>
+        <p class="text-center" data-aos="fade-up" style="font-weight: 500; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Our new patented technology will usher in a new Era in the Entertainment Industry. The product comes with a multifaceted functionality and dexterity, offering you Magical experience during play; features are specially designed for Your comfort.
+
+          We are currently developing the Minimum Viable Product (MVP) of the Electronic Talking Drum for a selected group of Testers, who will affirm the usability and functionality of the product. 
+          </p>
+      </div>
+    </section>
+
+    <!-- ======= CTA Section ======= -->
+    <section class="section cta-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
+            <h2>Fill Our Survey<br><h5 class="mt-3" style="text-transform: capitalize;color: beige;">Join our wait list to receive regular updates towards our first Pilot. </h5></h2>
+          </div>
+          <div class="col-md-5 text-center text-md-end">
+            <p><a href="https://docs.google.com/forms/d/1fDyUjn1NFKaxiO0f6JMMW_y8lx3SgbirR5ajEtW_Cc4/edit?usp=drivesdk" class="btn d-inline-flex align-items-center" style="background-color: rgb(12, 110, 12);font-weight: bold;"><span>TAKE SURVEY</span></a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+ 
+      <div class="row justify-content-center text-center mt-5">
+        <div class="col-md-7">
+          <p class="copyright">&copy; IntelligentsiaOrb. All Rights Reserved</p>
+          <div class="credits">
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=SoftLand
+          -->
+            <!-- Designed by <a href="https://pietech.com/">Pietech</a> -->
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" integrity="sha512-A7AYk1fGKX6S2SsHywmPkrnzTZHrgiVT7GcQkLGDe2ev0aWb8zejytzS8wjo7PGEXKqJOrjQ4oORtnimIRZBtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/10.3.1/swiper-bundle.min.js" integrity="sha512-2w85qGM9apXW9EgevsY4S4fnJIUz6U6mXlLbgDKphBuwh7jPQNad70Ll5W+pcIrJ6rIMGpjP0CxYGQwKsynIaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
